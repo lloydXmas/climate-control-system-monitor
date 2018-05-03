9628e870c1f5cdede659d8b34563a4cce465c871
Ziqing Wang <redtaq@hotmail.com>
2018-05-02 22:45:31 -0500

    detailed page 50%

--- controller.py
+++ controller.py
@@ -4,6 +4,7 @@ import tornado.web
 import tornado.log
 import time
 import requests
+import queries
 
 from jinja2 import \
     Environment, PackageLoader, select_autoescape
@@ -20,6 +21,10 @@ class TemplateHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Cache-Control',
       'no-store, no-cache, must-revalidate, max-age=0')
+      
+    def initialize(self):
+        self.session = queries.Session(
+        'postgresql://postgres@localhost:5432/home_mon')  
         
 class MainHandler(TemplateHandler):
     def get(self):
@@ -31,16 +36,34 @@ class MainHandler(TemplateHandler):
         payload = {'api_key': api_key, 'timezone': timezone}
         r = requests.get(url, params=payload)
         self.render_template("main.html", {'response': r.json()})
-        
+
+# after searching online, I've found dynamic SQL query is risky and a bad practice, so not use here
 class DetailHandler(TemplateHandler):
-    def get(self):
+    def get(self, slug, date):
         super().get()
-        self.render_template("detail_template.html", {})
+        fieldTemp = "{}_temp".format(slug)
+        fieldHumi = "{}_humidity".format(slug)
+        if slug == 'bedroom':
+            details = self.session.query('''SELECT created::time, bedroom_temp, bedroom_humidity FROM home_mon WHERE created::date = %(date)s ORDER BY created ASC ''', {'date': date})
+        elif slug == 'livingroom':
+            details = self.session.query('''SELECT created::time, livingroom_temp, livingroom_humidity FROM home_mon WHERE created::date = %(date)s ORDER BY created ASC ''', {'date': date})
+        elif slug == 'office':
+            details = self.session.query('''SELECT created::time, office_temp, office_humidity FROM home_mon WHERE created::date = %(date)s ORDER BY created ASC ''', {'date': date})
+        elif slug == 'kitchen':
+            details = self.session.query('''SELECT created::time, kitchen_temp, kitchen_humidity FROM home_mon WHERE created::date = %(date)s ORDER BY created ASC ''', {'date': date})
+        details.free()
+        temps = []
+        humis = []
+        for detail in details:
+            temps.append(float(detail[fieldTemp]))
+            humis.append(float(detail[fieldHumi]))
+        self.render_template("detail_template.html", {'slug': slug, 'temps': temps, 'humis': humis, 'date': date})
+    
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/detail", DetailHandler),
+        (r"/detail/(.*?)/(.*)", DetailHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
         {'path': 'monitor/static'})
         ],
---
+++ monitor/static/js/detail_charts.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    var figure_id = $("#detail-data").data()['slug'] + '_detail';
+    var detail_temps = $("#detail-data").data()['temp'];
+    var detail_humis = $("#detail-data").data()['humi'];
+    console.log(detail_temps);
+    console.log(detail_humis);
+    
+    // for ( let i = 0; i < detail_data.length; i ++) {
+    //     console.log(detail_data[i]);
+    // }
+    // draw highcharts    
+    function drawChart(id, temp, humidity) {
+        Highcharts.chart(id, {
+            chart: {
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Daily Room Climate'
+            },
+            subtitle: {
+                text: 'Source: <a href="https://thingspeak.com/channels/484266/shared_view">ThingSpeak Channel</a>'
+            },
+            xAxis: [{
+                categories: ['00:00','00:30','01:00','01:30','02:00','02:30','03:00','03:30','04:00','04:30','05:00','05:30','06:00','06:30','07:00','07:30','08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00','20:30','21:00','21:30','22:00','22:30','23:00','23:30'],
+                crosshair: true
+            }],
+            yAxis: [{ // Primary yAxis
+                min: 65,
+                max: 80,
+                labels: {
+                    format: '{value}°F',
+                    style: {
+                        color: '#3d76d3'
+                    }
+                },
+                title: {
+                    text: 'Temperature',
+                    style: {
+                        color: '#3d76d3'
+                    }
+                },
+
+            }, { // Secondary yAxis
+                min: 35,
+                max: 70,
+                gridLineWidth: 0,
+                title: {
+                    text: 'Humidity',
+                    style: {
+                        color: '#79dd5a'
+                    }
+                },
+                opposite: true,
+                labels: {
+                    format: '{value} %',
+                    style: {
+                        color: '#79dd5a'
+                    }
+                }
+
+            }],
+            tooltip: {
+                shared: true
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                x: -50,
+                verticalAlign: 'bottom',
+                y: -50,
+                floating: true,
+                backgroundColor: 'rgba(255,255,255,0.5)'
+            },
+            series: [{
+                name: 'Temperature',
+                type: 'spline',
+                
+                data: temp,
+                tooltip: {
+                    valueSuffix: ' °F'
+                },
+                color: '#3d76d3'
+
+            }, {
+                name: 'Humidity',
+                type: 'spline',
+                yAxis: 1,
+                data: humidity,
+                tooltip: {
+                    valueSuffix: ' %'
+                },
+                color: '#79dd5a'
+            }]
+        });
+    }
+
+    drawChart(figure_id, detail_temps, detail_humis);
+
+
+});
